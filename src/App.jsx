182f6d7c375bdc9ec4063d1fceba3bcc9dcc9583
Thomas <thomasfindlay94@gmail.com>
2021-09-21 11:13:25 +0100

    Added get user

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useRef, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { listUsers } from './api/usersApi';
+import React, { useEffect, useRef, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { getUser, listUsers } from "./api/usersApi";
 
 function App() {
   const [users, setUsers] = useState([]);
@@ -9,7 +9,7 @@ function App() {
 
   const fetchUsers = async () => {
     try {
-      console.log('abort?', abortRef.current);
+      console.log("abort?", abortRef.current);
       // abortRef.current?.();
       const response = await listUsers({
         abort: abort => {
@@ -19,12 +19,17 @@ function App() {
       console.log(response);
       setUsers(response);
     } catch (error) {
-      console.log('err', error.message, Object.keys(error));
+      console.log("err", error.message, Object.keys(error));
       console.error(error);
     }
   };
   useEffect(() => {
     fetchUsers();
+
+    (async () => {
+      const result = await getUser("u1WmOvm6MgQO5H0PbHPv");
+      console.log("user", result);
+    })();
   }, []);
 
   return (
--- src/api/usersApi.js
+++ src/api/usersApi.js
@@ -6,12 +6,24 @@ const extractDocs = async promise => {
   return result.docs.map(snap => snap.data());
 };
 
+const extractDoc = async promise => {
+  const result = await promise;
+  if (result.empty) return null;
+  return result.data();
+};
+
+const usersCollection = api.firestore.collection("users");
+
 export const listUsers = () => {
-  return extractDocs(api.firestore.collection("users").get());
+  return extractDocs(usersCollection.get());
+};
+
+export const getUser = id => {
+  return extractDoc(usersCollection.doc(id).get());
 };
 
 export const addUser = user => {
-  const doc = api.firestore.collection("users").doc();
+  const doc = usersCollection.doc();
   return doc.add({
     id: doc.id,
     ...user,
